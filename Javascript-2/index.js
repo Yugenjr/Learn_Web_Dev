bfeccb95b899bcb2a03b32f85202bab252865608
Yugen JR <yugenjr847@gmail.com>
2026-06-25 11:01:01 +0530

    js learn from home\

---
+++ Javascript-2/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World"); //single-line string
+console.log(`This is a multi-line string
+that spans across multiple lines.`);  //multi-line string
+
+
+window.alert("This is an alert");    //alert box
+/*
+
+multiline comment
+*/
+
+document.getElementById("myh1").innerHTML = "This is a heading"; //DOM manipulation
+document.getElementById("myp").textContent = "This is a paragraph"; 
